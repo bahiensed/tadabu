a3c008f15f29a925950ec0a1593ff3f9c7e3144d
dbahiense <douglas@rohling.com.br>
2024-09-30 04:26:24 -0300

    feat: now from 1 to 9

---
+++ src/app/9/page.tsx
@@ -0,0 +1,9 @@
+import Multiplication from "@/components/Multiplication";
+
+const page = () => {
+  return (
+    <Multiplication />
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,33 @@ export default function Home() {
         <a href="./1">
           <div className="shadow-2xl border-8 border-red-500 bg-red-100 w-full aspect-square text-red-600 flex text-6xl justify-center items-center hover:scale-105">x 1</div>
         </a>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center border-blue-500 bg-blue-100"></div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-yellow-500 bg-yellow-100 text-yellow-600">x 2</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-green-500 bg-green-100 text-green-600">x 3</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-orange-500 bg-orange-100 text-orange-600">x 4</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-purple-500 bg-purple-100 text-purple-600">x 5</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-red-500 bg-red-100 text-red-600">x 6</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-blue-500 bg-blue-200 text-blue-600">x 7</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-yellow-500 bg-yellow-100 text-yellow-600">x 8</div>
-        <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-green-500 bg-green-100 text-green-600">x 9</div>
+        <a href="./2">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-blue-500 bg-blue-200 text-blue-600">x 2</div>
+        </a>
+        <a href="./3">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-yellow-500 bg-yellow-100 text-yellow-600">x 3</div>
+        </a>
+        <a href="./4">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-green-500 bg-green-100 text-green-600">x 4</div>
+        </a>
+        <a href="./5">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-orange-500 bg-orange-100 text-orange-600">x 5</div>
+        </a>
+        <a href="./6">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-purple-500 bg-purple-100 text-purple-600">x 6</div>
+        </a>
+        <a href="./7">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-red-500 bg-red-100 text-red-600">x 7</div>
+        </a>
+        <a href="./8">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-blue-500 bg-blue-200 text-blue-600">x 8</div>
+        </a>
+        <a href="./9">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-6xl justify-center items-center hover:scale-105 border-yellow-500 bg-yellow-100 text-yellow-600">x 9</div>
+        </a>
+        <a href="./">
+          <div className="shadow-2xl border-8 w-full aspect-square flex text-xl justify-center items-center hover:scale-105 border-green-500 bg-green-100 text-green-600">Random</div>
+        </a>
       </div>
 
       </div>
